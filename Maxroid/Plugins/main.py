b815138b338ac3946dd68bdfc7a1d60a0bcd133b
{🇮🇳} シヴァ <xemishra@gmail.com>
2024-11-17 23:05:22 +0530

    Update main.py

--- Maxroid/Plugins/main.py
+++ Maxroid/Plugins/main.py
@@ -7,11 +7,11 @@ from Maxroid.core.clients import(
 
 @Maxroid.on_message(filters.command("vc"))
 async def join_voice_chat(client: Client, message: Message):
-    if message.chat.type == "supergroup":
-        group_call = await xemishra.join_group_call(
+    try:
+        await xemishra.join_group_call(
             chat_id=message.chat.id,
             stream_type="audio",
         )
         await message.reply("Joined the voice chat!")
-    else:
+    except:
         await message.reply("Please use this command in a group with a voice chat.")
