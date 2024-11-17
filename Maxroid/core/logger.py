fa1d08e7f80a7a7b3bb61a814e330a74123bb4e6
{🇮🇳} シヴァ <xemishra@gmail.com>
2024-11-17 22:45:09 +0530

    Create logger.py

---
+++ Maxroid/core/logger.py
@@ -0,0 +1,12 @@
+import logging
+
+logging.basicConfig(
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[logging.FileHandler("Maxroid.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("pytgcalls").setLevel(logging.ERROR)
+
+LOGGER = logging.getLogger("MaxroidMusicBot")
